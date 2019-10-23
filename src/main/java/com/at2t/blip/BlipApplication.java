cebd397fe83b135beda9beb78c47330bbb3c658d
Praveen Vatambeti <praveen@nineleaps.com>
2019-10-23 15:35:07 +0530

    Initial commit

---
+++ src/main/java/com/at2t/blip/BlipApplication.java
@@ -0,0 +1,13 @@
+package com.at2t.blip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/at2t/blip/BlipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.at2t.blip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
