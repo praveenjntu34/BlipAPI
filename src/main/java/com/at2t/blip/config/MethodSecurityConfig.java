73379a68031a6fed5021258a2eedf36cedd6ca38
Praveen <praveenjntu34@gmail.com>
2019-11-19 21:55:41 +0530

    Added method security

---
+++ src/main/java/com/at2t/blip/config/MethodSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.at2t.blip.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+}
