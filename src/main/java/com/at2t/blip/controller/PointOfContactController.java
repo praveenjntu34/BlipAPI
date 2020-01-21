1455faa05bad12eaeac3373b27cc97481ac9b9e6
Praveen Vatambeti <praveen@nineleaps.com>
2020-01-21 17:01:55 +0530

    changes

--- src/main/java/com/at2t/blip/controller/PointOfContactController.java
+++ src/main/java/com/at2t/blip/controller/PointOfContactController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/institution/poc")
+@RequestMapping
 @Api(value = "blip")
 public class PointOfContactController {
     @Autowired
